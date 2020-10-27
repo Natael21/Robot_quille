9a68b0be7650cbfd68fc19a7c7c1ab512b98d7d8
florencemillette <44734330+florencemillette@users.noreply.github.com>
2020-10-27 14:27:15 -0400

    sonnar robot quille

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <LibRobus.h>
+
+
+
+//float SONAR_GetRange(uint8_t 0); un seul sonnar, dans port 1
+
+
+
+void setup() 
+{
+  BoardInit();
+}
+
+void loop() {
+
+  
+}
